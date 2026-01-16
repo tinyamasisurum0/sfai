559d91f9ae4a625bb697646bb3cb10fd739d1fc3
ERCAN MURAT KISACA <tinyamasisurum0@gmail.com>
2026-01-16 11:04:43 +0300

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next'
 import { BLOG_POSTS } from '@/constants'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://sfai.netlify.app'
+  const baseUrl = 'https://data360sf.netlify.app'
   
   const routes = [
     '',
